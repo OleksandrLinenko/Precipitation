eae0892373d3ec684f4a22183b12f67ce3b53df6
oleksandrlinenko <oleksandrlinenko@localhost>
2025-07-15 14:06:00 +0200

    Add ShowMaxDayPrecipCommand class

---
+++ src/Precipitation/ShowMaxDayPrecipCommand.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Precipitation;
+
+import userinterface.Message;
+
+/**
+ *
+ * @author oleksandrlinenko
+ */
+public class ShowMaxDayPrecipCommand {
+    public static ShowMaxDayPrecipCommand create() {
+        return new ShowMaxDayPrecipCommand();
+    }
+    
+    public void handle() {
+        Message.create().show(MaxDayPrecip.create().handle(Application.getInstance().getMeasurements()));
+    }
+}
